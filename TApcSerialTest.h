f536ef1e509de0f404c39db9bbe2fcdbe6fac77d
RemizDP <avendor.rem@gmail.com>
2023-07-10 17:58:14 +0300

    git требует какой-то коммит

--- TApcSerialTest.h
+++
@@ -1,7 +0,0 @@
-class TApcSerialTest{
-public:
-  int openTest(const std::string astrPortName);
-  int baudrateTest(const std::string astrPortName, baud_t adwBaudRate);
-  int sendTest(const std::string astrPortName, baud_t adwBaudRate, uint32_t adwWsize, uint32_t adwRsize, uint32_t adwWriteTimeout, uint32_t adwReadTimeout);
-  int run(const std::string astrPortName, baud_t adwBaudRate);
-};
